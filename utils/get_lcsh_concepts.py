3121195668a1ef7bc3346b61f6056e1cb35e4b59
Bruce D'Arcus <bdarcus@gmail.com>
2008-07-13 11:36:37 -0400

    added little utility to grab lcsh.info triples

---
+++ utils/get_lcsh_concepts.py
@@ -0,0 +1,28 @@
+
+from rdflib import URIRef, ConjunctiveGraph
+
+concepts = [
+    "http://lcsh.info/sh85026205#concept", 
+    "http://lcsh.info/sh98000521#concept",	
+    "http://lcsh.info/sh85054192#concept",
+    "http://lcsh.info/sh91005953#concept",
+    "http://lcsh.info/sh99011194#concept",
+    "http://lcsh.info/sh85127474#concept",
+    "http://lcsh.info/sh85081863#concept",
+    "http://lcsh.info/sh85081871#concept",
+    "http://lcsh.info/sh85075119#concept",
+    "http://lcsh.info/sh89001033#concept",
+    "http://lcsh.info/sh85075801#concept",
+    "http://lcsh.info/sh89001287#concept"
+]
+
+graph = ConjunctiveGraph()
+graph.bind('skos', URIRef('http://www.w3.org/2004/02/skos/core#'))
+
+for concept in concepts:
+    print "parsing " + concept
+    graph.parse(URIRef(concept))
+
+print "\ngraph has " + str(len(graph)) + " triples."
+
+graph.serialize('mylcsh.rdf')
